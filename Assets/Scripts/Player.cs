2c0c4dd4ce296821dac71f6997fea914bfe77c71
sanysdl <sanysdl@gmail.com>
2018-12-06 22:06:34 -0200

    Press_start_V1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+public float fastmove;
+	void Start () {
+			
+	}
+	
+	void Update () {
+		Rigidbody2D rididbody = GetComponent<Rigidbody2D>();	
+		//movimento_horizontal
+			float move = Input.GetAxis("Horizontal");
+			rididbody.velocity = new Vector2(move*fastmove,rididbody.velocity.y);	
+		//jump
+			if(Input.GetKeyDown(KeyCode.Space))
+			{rididbody.AddForce(new Vector2(0,200));}
+}}
